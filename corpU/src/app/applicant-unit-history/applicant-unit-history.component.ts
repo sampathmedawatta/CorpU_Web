b5e7462ecb5beaad3b87893c2de1d367843c0213
kush0367 <kushsharma16a@gmail.com>
2023-05-11 20:04:26 +1000

    Created Application History for Applicant

---
+++ corpU/src/app/applicant-unit-history/applicant-unit-history.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-applicant-unit-history',
+  templateUrl: './applicant-unit-history.component.html',
+  styleUrls: ['./applicant-unit-history.component.css']
+})
+export class ApplicantUnitHistoryComponent {
+
+}
