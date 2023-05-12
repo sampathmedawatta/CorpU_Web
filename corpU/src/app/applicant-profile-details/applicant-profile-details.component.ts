83a11792c8238afb7b38edb2f39a38d7f389f511
kush0367 <kushsharma16a@gmail.com>
2023-05-12 17:45:50 +1000

    Creation of applicant personal details component

---
+++ corpU/src/app/applicant-profile-details/applicant-profile-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-applicant-profile-details',
+  templateUrl: './applicant-profile-details.component.html',
+  styleUrls: ['./applicant-profile-details.component.css']
+})
+export class ApplicantProfileDetailsComponent {
+
+}
