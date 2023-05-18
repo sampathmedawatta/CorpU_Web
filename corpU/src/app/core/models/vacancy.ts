5754ed042960c4febb74d661fa1dcc5419be1454
Sam Medawatta <sampath.medawatta@gmail.com>
2023-05-18 15:43:44 +1000

    add vacancy function

--- corpU/src/app/core/models/vacancy.ts
+++ corpU/src/app/core/models/vacancy.ts
@@ -19,16 +19,5 @@ export class vacancy{
     closingDate: string;
     status: boolean;
 
-    constructor(vacancy: any) {
-        this.vacancyId = vacancy.vacancyId;
-        this.vacancyTypeId = vacancy.vacancyTypeId;
-        this.classTypeId = vacancy.classTypeId;     
-        this.empId = vacancy.empId;     
-        this.unitId  = vacancy.unitId;
-        this.title  = vacancy.title;
-        this.description  = vacancy.description;
-        this.publishDate  = vacancy.publishDate;
-        this.closingDate  = vacancy.closingDate;
-        this.status  = vacancy.status;
-      }
+   
 }
