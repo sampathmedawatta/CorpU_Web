5472e5159ca3cb834fc01a5a9e5a48de9ea37043
Sam Medawatta <sampath.medawatta@gmail.com>
2023-05-19 13:35:13 +1000

    fixed duplicate header issue

--- corpU/src/app/components/shared/home-page/home-page.component.ts
+++ corpU/src/app/components/shared/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core';
 
 @Component({
   selector: 'app-home-page',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class HomePageComponent {
 
+constructor(private router: Router,private authService : AuthService){
+
+}
+
+  ngOnInit(): void {
+    
+    if(this.authService.isUserPermanentStaff()){
+      this.router.navigateByUrl('/Dashboard');
+    }
+    else if(this.authService.isUserApplicant()){
+      this.router.navigateByUrl('/Applicant');
+    }
+    
+  }
 }
