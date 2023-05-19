2267f105c40f6aeb5229c999177c28afd148dd42
Sam Medawatta <sampath.medawatta@gmail.com>
2023-05-19 23:23:30 +1000

    application list

--- corpU/src/app/core/models/application.ts
+++ corpU/src/app/core/models/application.ts
@@ -1,15 +1,11 @@
+import { applicant } from "./applicant";
+
 export class application{
-    ApplicationId: number;
+    applicationId: number;
     applicantId: number;
     vacancyId: number;
     resumeUrl: string;
     status: boolean;
-    
-    constructor(app: any) {
-        this.ApplicationId = app.ApplicationId;
-        this.applicantId = app.applicantId;
-        this.vacancyId = app.vacancyId;     
-        this.resumeUrl = app.resumeUrl;     
-        this.status  = app.status;
-      }
+    applicant : applicant;
+
 }
